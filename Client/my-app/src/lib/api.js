d1db8b4aa6bb1a301e6c72ba728b13a7d23578a7
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-10-30 18:26:36 +0200

    Started All-flights page

---
+++ Client/my-app/src/lib/api.js
@@ -0,0 +1,12 @@
+// const DOMAIN = ""; // Whatever the API domain is
+
+export async function getAllFlights() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not fetch quotes.");
+  }
+
+  return data;
+}
