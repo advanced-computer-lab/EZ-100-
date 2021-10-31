e67e6880df2d69e5bbea8c594fa2449bc3a50495
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-10-31 22:35:28 +0200

    Update Seeder.js

--- Server/Seeder.js
+++ Server/Seeder.js
@@ -18,10 +18,22 @@ const flights = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/flights.json`, "utf-8")
 );
 
+let transformedFlights = [];
+for (let i = 0; i < flights.length - 2; i += 3) {
+  const flightObj = {
+    ...flights[i],
+    EconomySeats: flights[i]["Seats Available on Flight"],
+    BusinessSeats: flights[i + 1]["Seats Available on Flight"],
+    FirstSeats: flights[i + 2]["Seats Available on Flight"],
+  };
+
+  transformedFlights.push(flightObj);
+}
+
 // Import data into DB
 const importData = async () => {
   try {
-    await Flight.create(flights);
+    await Flight.create(transformedFlights);
     console.log("Data imported...");
     process.exit();
   } catch (error) {
