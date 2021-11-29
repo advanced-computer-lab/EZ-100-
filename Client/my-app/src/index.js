b36c840de58cb71f1d06d29181f09cc97cc3e060
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-11-29 20:56:36 +0200

    Added AuthContext

--- Client/my-app/src/index.js
+++ Client/my-app/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { AuthContextProvider } from "./store/auth-context";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <AuthContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthContextProvider>,
   document.getElementById("root")
 );
