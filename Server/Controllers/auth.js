849358e9aff54b8fb5a24cb8d29baa59cd391564
joezzz21 <sofa-2000@hotmail.com>
2021-12-19 15:13:21 +0200

    auth updated

--- Server/Controllers/auth.js
+++ Server/Controllers/auth.js
@@ -43,30 +43,3 @@ exports.login = asyncHandler(async (req,res,next) => {
 
   res.status(200).json({ success: true, data: user, token });
 });
-
-
-
-exports.editUserInfo = asyncHandler(async (req, res, next) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!user) {
-    return next(new ErrorResponse(`User with ID ${userID} is not found`, 404));
-  }
-
-  res.status(200).json({ success: true, data: user });
-});
-
-exports.getUserById = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-
-  if (!user) {
-    return next(new ErrorResponse(`User with ID ${userID} is not found`, 404));
-  }
-
-  res.status(200).json({ success: true, data: user });
-});
-
-
