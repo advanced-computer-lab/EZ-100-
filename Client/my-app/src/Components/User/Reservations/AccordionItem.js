f1307004ea5cec7b5b194a5d6476fad0affdbb17
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-12-24 22:59:45 +0200

    Added Email summary - Frontend

--- Client/my-app/src/Components/User/Reservations/AccordionItem.js
+++ Client/my-app/src/Components/User/Reservations/AccordionItem.js
@@ -3,91 +3,30 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SendIcon from "@mui/icons-material/Send";
 
 import classes from "./AccordionItem.module.css";
 import FlightCard from "./FlightCard";
 
 export const AccordionItem = (props) => {
+  const [loading, setLoading] = React.useState(false);
+
   const reservation = props.reservation;
   console.log(reservation);
-  //   let departureDate = new Date(reservation.departureFlight.DepartureDate);
-  //   let arrivalDate = new Date(reservation.departureFlight.ArrivalDate);
-
-  //   let departureDate2 = new Date(reservation.arrivalFlight.DepartureDate);
-  //   let arrivalDate2 = new Date(reservation.arrivalFlight.ArrivalDate);
-
-  //   const options = {
-  //     year: "numeric",
-  //     month: "numeric",
-  //     day: "numeric",
-  //   };
-
-  //   departureDate = {
-  //     time: departureDate.toLocaleTimeString([], {
-  //       hour: "2-digit",
-  //       minute: "2-digit",
-  //     }),
-  //     longDate: departureDate.toLocaleDateString("en-US", options),
-  //   };
-
-  //   arrivalDate = {
-  //     longDate: arrivalDate.toLocaleDateString("en-US", options),
-  //     time: arrivalDate.toLocaleTimeString([], {
-  //       hour: "2-digit",
-  //       minute: "2-digit",
-  //     }),
-  //   };
-
-  //   departureDate2 = {
-  //     time: departureDate2.toLocaleTimeString([], {
-  //       hour: "2-digit",
-  //       minute: "2-digit",
-  //     }),
-  //     longDate: departureDate2.toLocaleDateString("en-US", options),
-  //   };
-
-  //   arrivalDate2 = {
-  //     longDate: arrivalDate2.toLocaleDateString("en-US", options),
-  //     time: arrivalDate2.toLocaleTimeString([], {
-  //       hour: "2-digit",
-  //       minute: "2-digit",
-  //     }),
-  //   };
-
-  //   let temp;
-  //   let cabin;
-  //   let depPrice;
-  //   let returnPrice;
-  //   if (reservation.cabin === "Economy") {
-  //     temp = "E";
-  //     cabin = "Economy";
-  //     depPrice = reservation.departureFlight.EconomyPrice;
-  //     returnPrice = reservation.arrivalFlight.EconomyPrice;
-  //   } else if (reservation.cabin === "Business") {
-  //     temp = "B";
-  //     cabin = "Business";
-  //     depPrice = reservation.departureFlight.BusinessPrice;
-  //     returnPrice = reservation.arrivalFlight.BusinessPrice;
-  //   } else {
-  //     temp = "F";
-  //     cabin = "First class";
-  //     depPrice = reservation.departureFlight.FirstPrice;
-  //     returnPrice = reservation.arrivalFlight.FirstPrice;
-  //   }
-  //   let depSeats = [];
-  //   let returnSeats = [];
-  //   for (const seat of reservation.departureSeats) {
-  //     depSeats.push(temp + seat);
-  //   }
-
-  //   for (const seat of reservation.arrivalSeats) {
-  //     returnSeats.push(temp + seat);
-  //   }
 
   const { From, To } = reservation.departureFlight;
 
   const onCancelHandler = () => {
     props.onCancel(reservation._id);
+  };
+
+  const onEmailSummaryHandler = async () => {
+    setLoading(true);
+    await fetch(
+      `http://localhost:5000/api/reservations/email_me/${reservation._id}`
+    );
+    setLoading(false);
   };
 
   return (
@@ -105,9 +44,21 @@ export const AccordionItem = (props) => {
               </h3>
               <p>Booking id: {reservation._id}</p>
             </div>
-            <button type="button" className="btn" onClick={onCancelHandler}>
-              Cancel reservation
-            </button>
+            <div className={classes["actions-row"]}>
+              <button type="button" className="btn" onClick={onCancelHandler}>
+                Cancel reservation
+              </button>
+              <LoadingButton
+                // style={{ backgroundColor: "#01579b" }}
+                onClick={onEmailSummaryHandler}
+                endIcon={<SendIcon />}
+                loading={loading}
+                loadingPosition="end"
+                variant="contained"
+              >
+                Email summary
+              </LoadingButton>
+            </div>
           </div>
         </AccordionSummary>
         <AccordionDetails>
