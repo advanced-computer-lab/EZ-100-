9862b636e80713b842dc8f84110b03d07c9f6410
Abuelyazeed <abuelyazeed175@gmail.com>
2021-12-24 16:11:50 +0200

    Change Password

--- Server/Controllers/auth.js
+++ Server/Controllers/auth.js
@@ -2,15 +2,15 @@ const User = require("../Models/User");
 const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/ErrorResponse");
 
-exports.createUser = asyncHandler(async (req,res,next) => {
+exports.createUser = asyncHandler(async (req, res, next) => {
   const user = await User.create({
-    "firstName" : req.body.firstName,
-    "lastName" : req.body.lastName,
-    "passportNumber" : req.body.passportNumber,
-    "dateOfBirth" : req.body.dateOfBirth,
-    "gender" : req.body.gender,
-    "email" : req.body.email,
-    "password": req.body.password
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    passportNumber: req.body.passportNumber,
+    dateOfBirth: req.body.dateOfBirth,
+    gender: req.body.gender,
+    email: req.body.email,
+    password: req.body.password,
   });
 
   const token = user.getSignedJwtToken();
@@ -18,28 +18,44 @@ exports.createUser = asyncHandler(async (req,res,next) => {
   res.status(200).json({ success: true, data: user, token });
 });
 
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
 
-exports.login = asyncHandler(async (req,res,next) => {
-  const { email, password} = req.body;
-
-  if(!email || !password){
+  if (!email || !password) {
     return next(new ErrorResponse("Please provide an email and password", 400));
   }
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
-  if(!user){
+  if (!user) {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
 
   const isMatch = await user.matchPassword(password);
 
-  if(!isMatch){
+  if (!isMatch) {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
-
 
   const token = user.getSignedJwtToken();
 
   res.status(200).json({ success: true, data: user, token });
+});
+
+//Update Password
+//@route PUT api/auth/updatepassword
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id).select("+password");
+
+  //Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, data: user, token });
+  //sendTokenResponse(user, 200, res);
 });
--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -2,9 +2,8 @@ const express = require("express");
 const auth = require("../Controllers/auth");
 const router = express.Router();
 
-router.post("/register", auth.createUser)
+router.post("/register", auth.createUser);
 router.post("/login", auth.login);
-
-
+router.put("/changepassword/:id", auth.updatePassword);
 
 module.exports = router;
