13029f886450d269984129947cadc6a1b186701a
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-12-25 16:46:30 +0200

    Change password done

--- Client/my-app/src/Components/Layout/Navigation.js
+++ Client/my-app/src/Components/Layout/Navigation.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
 import Box from "@mui/material/Box";
-
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
@@ -10,12 +9,18 @@ import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 
+import Modal from "../UI/Modal";
+
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
+import { ChangePassword } from "../User/Account/ChangePassword";
 
 export const Navigation = () => {
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -31,105 +36,118 @@ export const Navigation = () => {
 
   const onChangePassword = () => {
     console.log("Change your password");
+    setModalIsOpen(true);
+  };
+
+  const onCloseModal = () => {
+    setModalIsOpen(false);
   };
 
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>EZ Travel</div>
-      <nav className={classes.nav}>
-        <ul>
-          {!authCtx.isLoggedIn && (
+    <>
+      {modalIsOpen && (
+        <Modal onClose={onCloseModal}>
+          <ChangePassword onCloseModal={onCloseModal}></ChangePassword>
+        </Modal>
+      )}
+
+      <header className={classes.header}>
+        <div className={classes.logo}>EZ Travel</div>
+        <nav className={classes.nav}>
+          <ul>
+            {!authCtx.isLoggedIn && (
+              <li>
+                <NavLink activeClassName={classes.active} to="/login">
+                  Login
+                </NavLink>
+              </li>
+            )}
+
+            {!authCtx.isLoggedIn && (
+              <li>
+                <NavLink activeClassName={classes.active} to="/register">
+                  Register
+                </NavLink>
+              </li>
+            )}
+
+            {authCtx.isLoggedIn && (
+              <li>
+                <NavLink activeClassName={classes.active} to="/home" exact>
+                  Home
+                </NavLink>
+              </li>
+            )}
+
+            {authCtx.isLoggedIn && authCtx.user.role === "admin" && (
+              <li>
+                <NavLink activeClassName={classes.active} to="/flights">
+                  Flights
+                </NavLink>
+              </li>
+            )}
+
             <li>
-              <NavLink activeClassName={classes.active} to="/login">
-                Login
+              <NavLink activeClassName={classes.active} to="/contact-us">
+                Contact us
               </NavLink>
             </li>
-          )}
 
-          {!authCtx.isLoggedIn && (
-            <li>
-              <NavLink activeClassName={classes.active} to="/register">
-                Register
-              </NavLink>
-            </li>
-          )}
+            {authCtx.isLoggedIn && (
+              <li>
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar
+                        alt="Remy Sharp"
+                        src="/static/images/avatar/2.jpg"
+                      />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    <MenuItem>
+                      <Typography textAlign="center">
+                        <NavLink
+                          style={{ textDecoration: "none", color: "#000" }}
+                          to="/edit-user"
+                        >
+                          Edit my information
+                        </NavLink>
+                      </Typography>
+                    </MenuItem>
+                    <MenuItem>
+                      <Typography onClick={onChangePassword} textAlign="center">
+                        Change password
+                      </Typography>
+                    </MenuItem>
 
-          {authCtx.isLoggedIn && (
-            <li>
-              <NavLink activeClassName={classes.active} to="/home" exact>
-                Home
-              </NavLink>
-            </li>
-          )}
-
-          {authCtx.isLoggedIn && authCtx.user.role === "admin" && (
-            <li>
-              <NavLink activeClassName={classes.active} to="/flights">
-                Flights
-              </NavLink>
-            </li>
-          )}
-
-          <li>
-            <NavLink activeClassName={classes.active} to="/contact-us">
-              Contact us
-            </NavLink>
-          </li>
-
-          {authCtx.isLoggedIn && (
-            <li>
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="/static/images/avatar/2.jpg"
-                    />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  <MenuItem>
-                    <Typography textAlign="center">
-                      <NavLink
-                        style={{ textDecoration: "none", color: "#000" }}
-                        to="/edit-user"
-                      >
-                        Edit my information
-                      </NavLink>
-                    </Typography>
-                  </MenuItem>
-                  <MenuItem>
-                    <Typography onClick={onChangePassword} textAlign="center">
-                      Change password
-                    </Typography>
-                  </MenuItem>
-
-                  <MenuItem>
-                    <Typography onClick={onLogoutHandler} textAlign="center">
-                      Logout
-                    </Typography>
-                  </MenuItem>
-                </Menu>
-              </Box>
-            </li>
-          )}
-        </ul>
-      </nav>
-    </header>
+                    <MenuItem>
+                      <Typography onClick={onLogoutHandler} textAlign="center">
+                        Logout
+                      </Typography>
+                    </MenuItem>
+                  </Menu>
+                </Box>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
---
+++ Client/my-app/src/Components/User/Account/ChangePassword.js
@@ -0,0 +1,131 @@
+import React, { useContext, useState } from "react";
+import AuthContext from "../../../store/auth-context";
+
+import LoadingSpinner from "../../UI/LoadingSpinner";
+
+import classes from "./ChangePassword.module.css";
+
+export const ChangePassword = (props) => {
+  const [currentValue, setCurrentValue] = useState("");
+  const [newValue, setNewValue] = useState("");
+  const [confirmNewValue, setConfirmNewValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const currentChangeHandler = (event) => {
+    setCurrentValue(event.target.value);
+  };
+
+  const newChangeHandler = (event) => {
+    setNewValue(event.target.value);
+  };
+
+  const confirmNewChangeHandler = (event) => {
+    setConfirmNewValue(event.target.value);
+  };
+
+  const authCtx = useContext(AuthContext);
+
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+
+    if (newValue !== confirmNewValue) {
+      setErrorMessage("(New password) must be equal to (Confirm new password)");
+      return;
+    }
+    setErrorMessage("");
+
+    setLoading(true);
+
+    const body = { currentPassword: currentValue, newPassword: newValue };
+    const response = await fetch(
+      `http://localhost:5000/api/auth/changePassword/${authCtx.user._id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    setLoading(false);
+    if (!data.success) {
+      setErrorMessage("Current password is incorrect");
+    } else {
+      setCurrentValue("");
+      setNewValue("");
+      setConfirmNewValue("");
+    }
+  };
+
+  const onCancelHandler = () => {
+    props.onCloseModal();
+  };
+
+  return (
+    <>
+      <h3 style={{ textAlign: "center" }}>Change your password</h3>
+      <hr></hr>
+
+      {loading && (
+        <div className="centered">
+          <LoadingSpinner></LoadingSpinner>
+        </div>
+      )}
+      {!loading && (
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <label>Current password</label>
+            <input
+              id="current"
+              type="password"
+              value={currentValue}
+              onChange={currentChangeHandler}
+            />
+          </div>
+
+          <div className={classes.row}>
+            <label>New password</label>
+            <input
+              id="new"
+              type="password"
+              value={newValue}
+              onChange={newChangeHandler}
+            />
+          </div>
+
+          <div className={classes.row}>
+            <label>Confirm New password</label>
+            <input
+              id="confirmNew"
+              type="password"
+              value={confirmNewValue}
+              onChange={confirmNewChangeHandler}
+            />
+          </div>
+
+          <div className={classes.row}>
+            <p style={{ color: "red", fontSize: "0.9rem" }}>{errorMessage}</p>
+          </div>
+
+          <div className={classes.actions}>
+            <button
+              onClick={onCancelHandler}
+              type="button"
+              style={{ marginRight: "1rem" }}
+              className="del-btn"
+            >
+              Cancel
+            </button>
+            <button type="submit" className="btn" onClick={onSubmitForm}>
+              Confirm change
+            </button>
+          </div>
+        </form>
+      )}
+    </>
+  );
+};
