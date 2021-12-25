16c3f7a3a32b1cbaf64e3fa2ebd193ab4863308b
Mohamed Rostom <mohamedrostom62@gmail.com>
2021-12-25 14:11:11 +0200

    Update Navigation.js

--- Client/my-app/src/Components/Layout/Navigation.js
+++ Client/my-app/src/Components/Layout/Navigation.js
@@ -1,15 +1,36 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import Box from "@mui/material/Box";
+
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 export const Navigation = () => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   const authCtx = useContext(AuthContext);
 
   const onLogoutHandler = () => {
     authCtx.logout();
+  };
+
+  const onChangePassword = () => {
+    console.log("Change your password");
   };
 
   return (
@@ -57,7 +78,54 @@ export const Navigation = () => {
 
           {authCtx.isLoggedIn && (
             <li>
-              <button onClick={onLogoutHandler}>Logout</button>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem>
+                    <Typography textAlign="center">
+                      <NavLink
+                        style={{ textDecoration: "none", color: "#000" }}
+                        to="/edit-user"
+                      >
+                        Edit my information
+                      </NavLink>
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Typography onClick={onChangePassword} textAlign="center">
+                      Change password
+                    </Typography>
+                  </MenuItem>
+
+                  <MenuItem>
+                    <Typography onClick={onLogoutHandler} textAlign="center">
+                      Logout
+                    </Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
             </li>
           )}
         </ul>
